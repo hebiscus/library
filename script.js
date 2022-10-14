0b1848c795968700583a74f249239e6c9d4d73c0
hebi <akikopoland@gmail.com>
2022-10-14 19:09:47 +0200

    if one of the form inputs is empty, book is not added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let myLibrary = [];
 const addBook = document.querySelector(".add-button");
 
+function checkEmptyFields() {
+    if (authorinput.value == "" && titleinput.value == "" && pagesvalue == "" && readstatusvalue == "") {
+        submitinput.disabled = true;
+    }
+}
+
 
 addBook.addEventListener("click", function () {
     const addform = document.querySelector(".add-form");
@@ -16,52 +22,57 @@ addBook.addEventListener("click", function () {
         authorinput.setAttribute("required", "");
      let titleinput = form.appendChild(document.createElement('input'));
         titleinput.setAttribute("type", "text");
-        authorinput.setAttribute("required", "");
+        titleinput.setAttribute("required", "");
     let pagesinput= form.appendChild(document.createElement('input'));
         pagesinput.setAttribute("type", "number");
-        authorinput.setAttribute("required", "");
+        pagesinput.setAttribute("required", "");
     let readstatusinput = form.appendChild(document.createElement('input'));
         readstatusinput.setAttribute("type", "checkbox");
-        authorinput.setAttribute("required", "");
+        readstatusinput.setAttribute("required", "");
     let submitinput = form.appendChild(document.createElement('input'));
         submitinput.setAttribute("type", "submit");
 
+
     submitinput.addEventListener("click", function () {
+        if (authorinput.value == "" || titleinput.value == "" || pagesinput.value == "") {
+            return;
+        }
+
         let authorvalue =  authorinput.value;
         let titlevalue =  titleinput.value;
         let pagesvalue =  pagesinput.value;
         let readstatusvalue =  readstatusinput.value;
     
-    const newBook = new Book(authorvalue, titlevalue, pagesvalue, readstatusvalue);
+        const newBook = new Book(authorvalue, titlevalue, pagesvalue, readstatusvalue);
 
-    myLibrary.push(newBook);
+        myLibrary.push(newBook);
 
-    function addtoshelf() {
-        addform.removeChild(addformdiv);
+        function addtoshelf() {
+            addform.removeChild(addformdiv);
 
-        const bookshelf = document.querySelector(".bookshelf");
-        const bookitem = document.createElement("div");
-        bookshelf.appendChild(bookitem);
-        bookitem.classList.add("bookitem");
-        
-        let bookauthor = bookitem.appendChild(document.createElement("p"));
-        let booktitle = bookitem.appendChild(document.createElement("p"));
-        let bookpages = bookitem.appendChild(document.createElement("p"));
-        let bookreadstatus = bookitem.appendChild(document.createElement("p"));
+            const bookshelf = document.querySelector(".bookshelf");
+            const bookitem = document.createElement("div");
+            bookshelf.appendChild(bookitem);
+            bookitem.classList.add("bookitem");
+            
+            let bookauthor = bookitem.appendChild(document.createElement("p"));
+            let booktitle = bookitem.appendChild(document.createElement("p"));
+            let bookpages = bookitem.appendChild(document.createElement("p"));
+            let bookreadstatus = bookitem.appendChild(document.createElement("p"));
 
-        bookauthor.innerText  = myLibrary[0].author;
-        booktitle.innerText = myLibrary[0].title;
-        bookpages.innerText = myLibrary[0].pages;
+            bookauthor.innerText  = myLibrary[0].author;
+            booktitle.innerText = myLibrary[0].title;
+            bookpages.innerText = myLibrary[0].pages;
 
-        if (myLibrary[0].readstatus == "on") {
-            bookreadstatus.innerText  = "read"
-        } else {
-            "not read";
+            if (myLibrary[0].readstatus == "on") {
+                bookreadstatus.innerText  = "read"
+            } else {
+                "not read";
+            }
         }
-    }
 
-    addtoshelf();
-    });
+        addtoshelf();
+        });
 });
 
 function Book(author, title, pages, readstatus) {
@@ -71,20 +82,20 @@ function Book(author, title, pages, readstatus) {
     this.readstatus = readstatus;
 }
 
-function BookShelf() {
-    const bookshelf = document.getElementsByClassName("bookshelf");
+// function BookShelf() {
+//     const bookshelf = document.getElementsByClassName("bookshelf");
 
-    for (const book in myLibrary) {
-       let bookposition = bookshelf.appendChild(document.createElement("div"));
-       let booktitle = bookposition.appendChild("p");
-       let bookauthor = bookposition.appendChild("p");
-       let bookpages = bookposition.appendChild("p");
-       let bookreadstatus = bookposition.appendChild("p")
+//     for (const book in myLibrary) {
+//        let bookposition = bookshelf.appendChild(document.createElement("div"));
+//        let booktitle = bookposition.appendChild("p");
+//        let bookauthor = bookposition.appendChild("p");
+//        let bookpages = bookposition.appendChild("p");
+//        let bookreadstatus = bookposition.appendChild("p")
 
-       booktitle.textContent = Book.title;
-       bookauthor.textContent = Book.author;
-       bookpages.textContent = Book.pages;
-       bookreadstatus.textContent = Book.readstatus;
-    }
-}
+//        booktitle.textContent = Book.title;
+//        bookauthor.textContent = Book.author;
+//        bookpages.textContent = Book.pages;
+//        bookreadstatus.textContent = Book.readstatus;
+//     }
+// }
 
